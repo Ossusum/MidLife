7eca627b606ca6ff0d40084c5b5cef1b4f23d098
klkwarrior <ossusum.omnination@gmail.com>
2016-11-16 14:04:52 -0500

    Initial Commit

---
+++ src/Logic/Entity.java
@@ -0,0 +1,10 @@
+package Logic;
+
+/**
+ * Created by Benny on 11/16/2016.
+ */
+public class Entity {
+    private Part[] bodyParts = {new Part("Head",10),new Part("Torso",30),new Part("Left Arm",15),new Part("Right Arm",15),new Part("Left Leg",15),new Part("Right Arm",15)};
+    private boolean isDead = false;
+
+}
